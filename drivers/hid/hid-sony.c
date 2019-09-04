bb9fefe1af86d176d54c37fe84380920069fe75e
Roderick Colenbrander <roderick.colenbrander@sony.com>
2019-09-04 14:22:11 -0700

    UPSTREAM: HID: sony: Fix memory corruption issue on cleanup.
    The sony driver is not properly cleaning up from potential failures in
    sony_input_configured. Currently it calls hid_hw_stop, while hid_connect
    is still running. This is not a good idea, instead hid_hw_stop should
    be moved to sony_probe. Similar changes were recently made to Logitech
    drivers, which were also doing improper cleanup.
    
    Signed-off-by: Roderick Colenbrander <roderick.colenbrander@sony.com>
    CC: stable@vger.kernel.org
    Signed-off-by: Jiri Kosina <jkosina@suse.cz>
    
    Bug: 139372370
    Change-Id: I97dadab125a6a426763efd3beb939ddca017c6b6
    (cherry picked from commit 2bcdacb70327013ca2066bfcf2af1009eff01f1d
    ("HID: sony: Fix memory corruption issue on cleanup."))
    Signed-off-by: Siarhei Vishniakou <svv@google.com>

--- drivers/hid/hid-sony.c
+++ drivers/hid/hid-sony.c
@@ -2705,7 +2705,6 @@ err_stop:
 	kfree(sc->output_report_dmabuf);
 	sony_remove_dev_list(sc);
 	sony_release_device_id(sc);
-	hid_hw_stop(hdev);
 	return ret;
 }
 
@@ -2764,6 +2763,7 @@ static int sony_probe(struct hid_device *hdev, const struct hid_device_id *id)
 	 */
 	if (!(hdev->claimed & HID_CLAIMED_INPUT)) {
 		hid_err(hdev, "failed to claim input\n");
+		hid_hw_stop(hdev);
 		return -ENODEV;
 	}
 
