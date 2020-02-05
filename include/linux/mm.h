2aed4830ead1e83d12cae85d08a460bfa1b371c1
Tyler Nijmeh <tylernij@gmail.com>
2020-02-04 21:41:14 -0800

    mm: Disable readahead by default
    An article from 2014 (old but relevant) discussed the impact of
    readahead on flash storage and its effects on SQLine and DBMS
    performance. What was discovered was that turning off readahead could
    improve performance and power consumption by 70% (sequential; 60% for
    random).
    
    https://www.semanticscholar.org/paper/Revisiting-read-ahead-efficiency-for-raw-NAND-flash-Olivier-Boukhobza/297f3d189dcd9b099f8f6b5fbc7e304db395a683
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2023,8 +2023,8 @@ int write_one_page(struct page *page, int wait);
 void task_dirty_inc(struct task_struct *tsk);
 
 /* readahead.c */
-#define VM_MAX_READAHEAD	128	/* kbytes */
-#define VM_MIN_READAHEAD	16	/* kbytes (includes current page) */
+#define VM_MAX_READAHEAD	0	/* kbytes */
+#define VM_MIN_READAHEAD	0	/* kbytes (includes current page) */
 extern int mmap_readaround_limit;
 
 int force_page_cache_readahead(struct address_space *mapping, struct file *filp,
