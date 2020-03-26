199759a1c297bfe2300cc848a0bfde6db6ded24a
BlackGunZ <mohkun25@gmail.com>
2020-03-26 22:54:25 +0100

    battery: max77854: Turn off debug Author: pascua28

--- drivers/battery/max77854_charger.c
+++ drivers/battery/max77854_charger.c
@@ -9,8 +9,6 @@
  * it under the terms of the GNU General Public License version 2 as
  * published by the Free Software Foundation.
  */
-#define DEBUG
-
 #include <linux/mfd/max77854-private.h>
 #if defined(CONFIG_BATTERY_NOTIFIER)
 #include <linux/battery/battery_notifier.h>
