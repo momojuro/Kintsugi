30121e0739bcdee4d70888c1f535527eb58a44b7
BlackGunZ <mohkun25@gmail.com>
2020-05-16 17:18:26 +0200

    Revert "cpufreq: exynos-acme: Increase undervolting to -100mV"
    This reverts commit 7287da1372d0395f432a6abadb5a65bc87d4962f.

--- drivers/cpufreq/exynos-acme.c
+++ drivers/cpufreq/exynos-acme.c
@@ -847,8 +847,8 @@ static __init int init_table(struct exynos_cpufreq_domain *domain)
 	for (index = 0; index < domain->table_size; index++) {
 		domain->freq_table[index].driver_data = index;
 
-		/* Undervolt by 100 mV */
-		volt_table[index] -= 100000;
+		/* Undervolt by 75 mV */
+		volt_table[index] -= 75000;
 
 		if (table[index] > domain->max_freq)
 			domain->freq_table[index].frequency = CPUFREQ_ENTRY_INVALID;
